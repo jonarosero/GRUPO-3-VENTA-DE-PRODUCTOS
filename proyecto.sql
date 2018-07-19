1ab683acc9832646beb4b006e930552ab4189975
Bruno Esparza <bruno.be81@gmail.com>
2018-07-19 16:41:31 -0500

    feat(*): add basic db with tables

---
+++ proyecto.sql
@@ -0,0 +1,261 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 19-07-2018 a las 23:40:16
+-- Versión del servidor: 10.1.32-MariaDB
+-- Versión de PHP: 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categories`
+--
+
+CREATE TABLE `categories` (
+  `CategoryID` tinyint(3) UNSIGNED NOT NULL,
+  `CategoryName` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `categories`
+--
+
+INSERT INTO `categories` (`CategoryID`, `CategoryName`) VALUES
+(1, 'Technology ');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `customers`
+--
+
+CREATE TABLE `customers` (
+  `CustomerID` varchar(10) NOT NULL,
+  `Name` varchar(30) DEFAULT NULL,
+  `Address` varchar(60) DEFAULT NULL,
+  `City` varchar(15) DEFAULT NULL,
+  `Country` varchar(15) DEFAULT NULL,
+  `Phone` varchar(24) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `customers`
+--
+
+INSERT INTO `customers` (`CustomerID`, `Name`, `Address`, `City`, `Country`, `Phone`) VALUES
+('1104113889', 'Bruno Esparza', 'Pradera, Alisos y Arupos', 'Loja', 'Loja', '0011223344'),
+('123', 'Test Cliente', 'Calle 1 y Calle 2', 'Loja', 'Loja', '1122334455');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `order details`
+--
+
+CREATE TABLE `order details` (
+  `OrderID` int(10) UNSIGNED NOT NULL,
+  `ProductID` smallint(5) UNSIGNED NOT NULL,
+  `UnitPrice` decimal(8,2) UNSIGNED NOT NULL DEFAULT '999999.99',
+  `Quantity` smallint(2) UNSIGNED NOT NULL DEFAULT '1',
+  `Discount` double(8,0) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `orders`
+--
+
+CREATE TABLE `orders` (
+  `OrderID` int(10) UNSIGNED NOT NULL,
+  `CustomerID` varchar(5) DEFAULT NULL,
+  `EmployeeID` mediumint(8) UNSIGNED NOT NULL,
+  `OrderDate` date DEFAULT NULL,
+  `RequiredDate` date DEFAULT NULL,
+  `ShippedDate` date DEFAULT NULL,
+  `ShipVia` tinyint(3) UNSIGNED DEFAULT NULL,
+  `Freight` decimal(10,2) UNSIGNED DEFAULT '0.00',
+  `ShipName` varchar(40) DEFAULT NULL,
+  `ShipAddress` varchar(60) DEFAULT NULL,
+  `ShipCity` varchar(15) DEFAULT NULL,
+  `ShipRegion` varchar(15) DEFAULT NULL,
+  `ShipPostalCode` varchar(10) DEFAULT NULL,
+  `ShipCountry` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE `products` (
+  `ProductID` smallint(5) UNSIGNED NOT NULL,
+  `ProductName` varchar(40) NOT NULL,
+  `SupplierID` smallint(5) UNSIGNED NOT NULL,
+  `CategoryID` tinyint(3) UNSIGNED NOT NULL,
+  `QuantityPerUnit` varchar(20) DEFAULT NULL,
+  `UnitPrice` decimal(10,2) UNSIGNED DEFAULT '0.00',
+  `UnitsInStock` smallint(6) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`ProductID`, `ProductName`, `SupplierID`, `CategoryID`, `QuantityPerUnit`, `UnitPrice`, `UnitsInStock`) VALUES
+(1, 'Iphone X', 1, 1, '1', '1200.00', 20),
+(2, 'Iphone X', 2, 1, '2', '1150.00', 10),
+(3, 'MacBook Pro', 1, 1, '1', '6999.00', 100);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `SupplierID` smallint(5) UNSIGNED NOT NULL,
+  `Name` varchar(40) NOT NULL,
+  `Address` varchar(60) DEFAULT NULL,
+  `City` varchar(15) DEFAULT NULL,
+  `Country` varchar(15) DEFAULT NULL,
+  `Phone` varchar(24) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `suppliers`
+--
+
+INSERT INTO `suppliers` (`SupplierID`, `Name`, `Address`, `City`, `Country`, `Phone`) VALUES
+(1, 'Apple', 'Apple Campus', 'Cupertino', 'California', '12345'),
+(2, 'Amazon', '20450 Stevens Creek Blvd.', 'Cupertino', 'California', '(408) 790-6400');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`CategoryID`),
+  ADD UNIQUE KEY `CategoryName` (`CategoryName`);
+
+--
+-- Indices de la tabla `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`CustomerID`),
+  ADD KEY `City` (`City`);
+
+--
+-- Indices de la tabla `order details`
+--
+ALTER TABLE `order details`
+  ADD PRIMARY KEY (`OrderID`,`ProductID`),
+  ADD KEY `ProductID` (`ProductID`);
+
+--
+-- Indices de la tabla `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`OrderID`),
+  ADD KEY `OrderDate` (`OrderDate`),
+  ADD KEY `ShippedDate` (`ShippedDate`),
+  ADD KEY `ShipPostalCode` (`ShipPostalCode`),
+  ADD KEY `CustomerID` (`CustomerID`),
+  ADD KEY `EmployeeID` (`EmployeeID`),
+  ADD KEY `ShipVia` (`ShipVia`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ProductID`),
+  ADD KEY `ProductName` (`ProductName`),
+  ADD KEY `CategoryID` (`CategoryID`),
+  ADD KEY `SupplierID` (`SupplierID`);
+
+--
+-- Indices de la tabla `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`SupplierID`),
+  ADD KEY `CompanyName` (`Name`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `CategoryID` tinyint(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `OrderID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `ProductID` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `SupplierID` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `order details`
+--
+ALTER TABLE `order details`
+  ADD CONSTRAINT `order details_ibfk_1` FOREIGN KEY (`OrderID`) REFERENCES `orders` (`OrderID`),
+  ADD CONSTRAINT `order details_ibfk_2` FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`);
+
+--
+-- Filtros para la tabla `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`),
+  ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`EmployeeID`) REFERENCES `employees` (`EmployeeID`),
+  ADD CONSTRAINT `orders_ibfk_3` FOREIGN KEY (`ShipVia`) REFERENCES `shippers` (`ShipperID`);
+
+--
+-- Filtros para la tabla `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`CategoryID`) REFERENCES `categories` (`CategoryID`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`SupplierID`) REFERENCES `suppliers` (`SupplierID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
